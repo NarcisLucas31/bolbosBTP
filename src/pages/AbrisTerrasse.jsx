cc669170712ddc40048601a5ac3966b5590064bc
NarcisLucas31 <131599851+NarcisLucas31@users.noreply.github.com>
2024-12-04 22:04:45 +0100

    Update AbrisTerrasse.jsx
    A en a

--- src/pages/AbrisTerrasse.jsx
+++ src/pages/AbrisTerrasse.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/navbar/Navbar";
 import Heroback from "../components/heroback/Heroback";
 import Footer from "../components/footer/Footer";
 import Avantnav from "../components/avantnav/Avantnav";
-import AbrisTerraseImg from "../assets/AbrisTerraseImg.webp";
+import abrisTerraseImg from "../assets/abrisTerraseImg.webp";
 import Apropos from "../components/apropos/Apropos";
 import aproposAbris from "../assets/aproposAbris.jpg";
 import aproposTerrasse from "../assets/aproposTerrasse.jpg";
