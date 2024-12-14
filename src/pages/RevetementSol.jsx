73066f0d9fd00cf4708b5e1df47241979faaeae2
NarcisLucas31 <131599851+NarcisLucas31@users.noreply.github.com>
2024-12-14 12:17:57 +0100

    plus que quelques pages et apres opitmisation

--- src/pages/RevetementSol.jsx
+++ src/pages/RevetementSol.jsx
@@ -1,25 +1,81 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
-import Hero from "../components/hero/Hero";
 import Titre from "../components/titre/Titre";
 import Footer from "../components/footer/Footer";
-import Doubleimage from "../components/doubleimage/Doubleimage";
-import Imagegauche from "../components/imagegauche/Imagegauche";
+import Apropos from "../components/apropos/Apropos";
+import Aproposdroite from "../components/aporposdroite/Aproposdroite";
 import Avantnav from "../components/avantnav/Avantnav";
+import Heroback from "../components/heroback/Heroback";
+import parquet1 from "../assets/parquet1.jpg";
+import parquet2 from "../assets/parquet2.jpg";
+import parquet3 from "../assets/parquet3.avif";
+import parquet4 from "../assets/parquet4.avif";
+import parquet5 from "../assets/parquet5.jpg";
+import parquet6 from "../assets/parquet6.jpg";
+import parquet7 from "../assets/parquet7.jpg";
 
 const RevetementSol = () => {
   return (
     <div>
       <Avantnav />
       <Navbar />
-      <Hero />
+      <Heroback
+        imageSrc={parquet1}
+        text="Revêtez votre intérieur avec des sols élégants et durables."
+        titre=""
+      />
       <div className="container">
+        <Titre subTitre="" titre="Parquet" />
+        <Apropos
+          imageSrc={parquet2}
+          text="Le parquet massif, composé entièrement de bois naturel, est un choix élégant et durable qui apporte chaleur et authenticité à 
+          votre intérieur. Il se distingue par sa longévité. Disponible dans une large gamme d’essences et de finitions, il s’adapte à tous les 
+          styles et reste un investissement de qualité pour des espaces de vie raffinés et intemporels."
+          titre="Parquet massif : Élégance et durabilité"
+        />
+        <Apropos
+          imageSrc={parquet3}
+          text="Le parquet stratifié offre l'apparence du bois à un prix abordable. Facile à entretenir, résistant aux rayures et disponible 
+          dans de nombreuses finitions, il s’adapte parfaitement à tous les styles et convient aux zones à fort passage."
+          titre="Parquet stratifié : Esthétique et pratique"
+        />
+        <Apropos
+          imageSrc={parquet4}
+          text="Le parquet contrecollé offre l'aspect du bois naturel avec une installation rapide et stable. Toutefois, sa couche de bois mince 
+          limite sa durabilité par rapport au parquet massif, et il peut être sensible à l'humidité."
+          titre="Parquet contrecollé : Pratique avec quelques limites"
+        />
         <Titre
-          subTitre="Tout votre aménagement, dedans comme dehors."
+          subTitre="Tous les différent type de parquets, qu'ils soient massifs, contrecollés ou stratifiés, offrent une grande 
+        variété de formes esthétiques et de tailles, pour s'adapter parfaitement à vos envies et à votre espace."
           titre=""
         />
-        <Doubleimage />
-        <Imagegauche />
+        <Titre subTitre="" titre="Parquet" />
+        <Aproposdroite
+          imageSrc={parquet5}
+          text="Le grès cérame est un revêtement de sol particulièrement résistant aux chocs, à l'humidité et à l'usure, ce qui le rend idéal 
+          pour les zones à fort passage. Il offre une large gamme de styles, avec des effets imitant le bois, le béton ou la pierre, permettant 
+          de s'adapter à tous les types d'intérieurs. Facile à entretenir, il combine esthétique, durabilité et praticité."
+          titre="Grès cérame : Résistance et esthétique variée"
+        />
+        <Aproposdroite
+          imageSrc={parquet6}
+          text="La faïence est idéale pour les murs, mais peut aussi être utilisée au sol pour un effet décoratif, surtout dans les cuisines 
+          et salles de bains. Disponible en différents motifs et finitions, elle allie esthétique et résistance à l’humidité."
+          titre="Faïence : Parfaite pour les murs, décorative au sol"
+        />
+        <Aproposdroite
+          imageSrc={parquet7}
+          text="La terre cuite offre une ambiance rustique et chaleureuse grâce à ses teintes naturelles. Idéale pour un style 
+          traditionnel, elle apporte du charme et de la convivialité, tout en étant durable et facile à entretenir. Parfaite pour les salons, 
+          cuisines ou halls, elle crée une atmosphère authentique et intemporelle."
+          titre="Terre cuite : Chaleur et authenticité"
+        />
+        <Titre
+          subTitre="Tous les différent type de carrelages, à l'exception de la terre cuite, sont disponibles dans une large gamme de motifs, 
+          de formes et de tailles, pour répondre à vos besoins esthétiques et fonctionnels."
+          titre=""
+        />
       </div>
       <Footer />
     </div>
--- src/pages/SalleDeBain.jsx
+++ src/pages/SalleDeBain.jsx
@@ -1,26 +1,46 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
-import Hero from "../components/hero/Hero";
-import Titre from "../components/titre/Titre";
 import Footer from "../components/footer/Footer";
-import Doubleimage from "../components/doubleimage/Doubleimage";
-import Imagegauche from "../components/imagegauche/Imagegauche";
+import Apropos from "../components/apropos/Apropos";
+import Aproposdroite from "../components/aporposdroite/Aproposdroite";
 import Avantnav from "../components/avantnav/Avantnav";
+import Heroback from "../components/heroback/Heroback";
+import salledebain1 from "../assets/salledebain1.webp";
+import salledebain2 from "../assets/salledebain2.webp";
+import salledebain3 from "../assets/salledebain3.jpg";
+import salledebain4 from "../assets/salledebain4.jpg";
 
 const salleDeBain = () => {
   return (
     <div>
       <Avantnav />
       <Navbar />
-      <Hero />
-      <div className="container">
-        <Titre
-          subTitre="Tout votre aménagement, dedans comme dehors."
-          titre=""
-        />
-        <Doubleimage />
-        <Imagegauche />
-      </div>
+      <Heroback
+        imageSrc={salledebain1}
+        text="Donnez vie à la salle de bain de vos rêves avec des solutions personnalisées."
+        titre=""
+      />
+      <Aproposdroite
+        imageSrc={salledebain2}
+        text="Optez pour une salle de bain qui allie esthétique et praticité grâce à des solutions sur mesure. Chaque détail est 
+        pensé pour refléter vos goûts tout en s’adaptant parfaitement à la configuration de votre espace. Maximisez le confort et la 
+        fonctionnalité avec un agencement intelligent et des équipements modernes, le tout conçu selon vos besoins."
+        titre="Design et fonctionnalité"
+      />
+      <Apropos
+        imageSrc={salledebain3}
+        text="Transformez votre salle de bain en un espace élégant et fonctionnel grâce à des matériaux de qualité supérieure et des finitions 
+        soignées. Optez pour des équipements modernes, comme des douches à l’italienne, des éclairages LED et des rangements astucieux, pour un 
+        design qui allie confort et innovation. De plus, notre processus d’installation rapide et organisé garantit un résultat impeccable dans 
+        les délais impartis, sans compromis sur la qualité."
+        titre="Matériaux haut de gamme et installation optimale"
+      />
+      <Aproposdroite
+        imageSrc={salledebain4}
+        text="Exploitez pleinement chaque mètre carré de votre salle de bain avec des meubles et agencements sur mesure. Que votre espace 
+        soit petit ou grand, nous créons des solutions personnalisées pour maximiser le rangement et le confort."
+        titre="Optimisation des espaces"
+      />
       <Footer />
     </div>
   );
